37c1104879799c01cf06d2450eabdd2426971b9b
Andrey Dolzhikov <eylau-andrew@mail.ru>
2023-11-20 14:04:52 +0200

    accessibility utils js

---
+++ accessibility-utils.js
@@ -0,0 +1,37 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.sendMessageToAssistiveTechnology = void 0;
+function sendMessageToAssistiveTechnology(message) {
+    if (!window.dxAccessibilityHelper)
+        window.dxAccessibilityHelper = new AccessibilityHelper();
+    window.dxAccessibilityHelper.sendMessageToAssistiveTechnology(message);
+}
+exports.sendMessageToAssistiveTechnology = sendMessageToAssistiveTechnology;
+var AccessibilityHelper = /** @class */ (function () {
+    function AccessibilityHelper() {
+        this._helperElement = null;
+    }
+    Object.defineProperty(AccessibilityHelper.prototype, "helperElement", {
+        get: function () {
+            if (this._helperElement == null)
+                this._helperElement = this.createHelperElement();
+            return this._helperElement;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    AccessibilityHelper.prototype.createHelperElement = function () {
+        var helperElement = document.createElement("DIV");
+        helperElement.className = "dxAIFE dxAIFME";
+        helperElement.setAttribute("role", "note");
+        helperElement.setAttribute("aria-live", "assertive");
+        document.documentElement.appendChild(helperElement);
+        return helperElement;
+    };
+    AccessibilityHelper.prototype.sendMessageToAssistiveTechnology = function (message) {
+        var _this = this;
+        this.helperElement.innerHTML = message;
+        setTimeout(function () { _this.helperElement.innerHTML = ""; }, 300);
+    };
+    return AccessibilityHelper;
+}());
